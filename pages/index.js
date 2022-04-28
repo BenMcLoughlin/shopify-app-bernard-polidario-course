d2d414265b9cc0ceb0c7bf315d618c0251100956
Ben McLoughlin <benmcl@shaw.ca>
2022-04-28 08:33:31 -0700

    changed remote origin

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components;
+
+
+export const Index = () => {
+return (
+        <div className="">
+             EXAMPLE SHOPIFY APP
+        </div>
+     );
+ };
+
+ export default Index
+ 
---
+++ server.js
@@ -0,0 +1,9 @@
+import "@babel/polyfill";
+import dotenv from "dotenv";
+import "isomorphic-fetch";
+import createShopifyAuth, { verifyRequest } from "@shopify/koa-shopify-auth";
+import Shopify, { ApiVersion } from "@shopify/shopify-api";
+import Koa from "koa";
+import next from "next";
+import Router from "koa-router";
+import axios from "axios";
